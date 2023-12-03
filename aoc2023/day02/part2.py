f0ed4c03384150b2dd35cd3151168ef96b095859
Christopher Jelly <christopher.p.jelly@gmail.com>
2023-12-03 16:38:34 -0500

    finished day 2 part 2

---
+++ aoc2023/day02/part2.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import re
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
+
+
+def check_if_impossible(cubes: str):
+    limits = {"red": 12, "green": 13, "blue": 14}
+    for cube in cubes:
+
+        count, color = int(cube.split(' ')[0]), cube.split(' ')[1]
+        if count > limits[color]:
+            return True
+    return False
+
+def return_minimum_possible(line: str):
+    red_cubes = [int(x) for x in re.findall(r'(\d+) red', line)]
+    min_red = max(red_cubes)
+    green_cubes = [int(x) for x in re.findall(r'(\d+) green', line)]
+    min_green = max(green_cubes)
+    blue_cubes = [int(x) for x in re.findall(r'(\d+) blue', line)]
+    min_blue = max(blue_cubes)
+    cube_powers = min_red*min_green*min_blue
+
+    return cube_powers
+
+
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    total = 0
+
+    for line in lines:
+        sum_min_cubes = return_minimum_possible(line=line)
+        total += sum_min_cubes
+
+    return total
+
+
+INPUT_S = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+EXPECTED = 2286
+
+
+@pytest.mark.parametrize(
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
