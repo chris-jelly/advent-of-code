1e069a587043a75a765cb41aeea432efe6f13ae1
Christopher Jelly <christopher.p.jelly@gmail.com>
2023-12-03 15:00:53 -0500

    finished day 2 part 1

--- aoc2023/day02/part1.py
+++ aoc2023/day02/part1.py
@@ -1,5 +1,7 @@
 from __future__ import annotations
 
+import re
+
 import argparse
 import os.path
 
@@ -7,32 +9,49 @@ import pytest
 
 import support
 
-INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+INPUT_TXT = os.path.join(os.path.dirname(__file__), "input.txt")
 
+
+def check_if_impossible(cubes: str):
+    limits = {"red": 12, "green": 13, "blue": 14}
+    for cube in cubes:
+
+        count, color = int(cube.split(' ')[0]), cube.split(' ')[1]
+        if count > limits[color]:
+            return True
+    return False
 
 def compute(s: str) -> int:
-    numbers = support.parse_numbers_split(s)
-    for n in numbers:
-        pass
-
     lines = s.splitlines()
+    total = 0
     for line in lines:
-        pass
-    # TODO: implement solution here!
-    return 0
+        game_id = int(line.split(" ")[1].split(':')[0])
+        list_games = line.split(";")
+        is_impossible = False
+        for rounds in list_games:
+            cubes = re.findall(r"\d+ \w+", rounds)
+            if check_if_impossible(cubes) is True:
+                is_impossible = True
+                break
+        if is_impossible is False:
+            total += game_id
+
+    return total
 
 
-INPUT_S = '''\
-
-'''
-EXPECTED = 1
+INPUT_S = """\
+Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+"""
+EXPECTED = 8
 
 
 @pytest.mark.parametrize(
-    ('input_s', 'expected'),
-    (
-        (INPUT_S, EXPECTED),
-    ),
+    ("input_s", "expected"),
+    ((INPUT_S, EXPECTED),),
 )
 def test(input_s: str, expected: int) -> None:
     assert compute(input_s) == expected
@@ -40,7 +59,7 @@ def test(input_s: str, expected: int) -> None:
 
 def main() -> int:
     parser = argparse.ArgumentParser()
-    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    parser.add_argument("data_file", nargs="?", default=INPUT_TXT)
     args = parser.parse_args()
 
     with open(args.data_file) as f, support.timing():
@@ -49,5 +68,5 @@ def main() -> int:
     return 0
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     raise SystemExit(main())
